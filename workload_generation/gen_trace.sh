650c8d571824c8ae552451143d3af7fcb2e61025
Michael Mercier <michael.mercier@inria.fr>
2016-02-01 11:18:15 +0100

    Debug gen_trace

--- workload_generation/gen_trace.sh
+++ workload_generation/gen_trace.sh
@@ -5,10 +5,10 @@ _script="$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
 EXTRAE_HOME=/opt/extrae
 NPB_BIN=/root/NPB3.3.1/NPB3.3-MPI/bin/
 
+OAR_NODEFILE=$_script/hostfile
 NBPROC=$(uniq $OAR_NODEFILE | wc -l)
 CMDS=$(ls -1 $NPB_BIN | grep -E "*\.$NBPROC$")
 RESULTS=/root/results/
-OAR_NODEFILE=$_script/hostfile
 
 mkdir -p $RESULTS
 cd $RESULTS
