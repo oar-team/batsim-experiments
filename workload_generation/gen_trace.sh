0aaa0aa1f3d04ffa71422ab6e1efdb3ce9f3c8f9
Michael Mercier <michael.mercier@inria.fr>
2016-02-01 11:12:22 +0100

    Debug gen-trace

--- workload_generation/gen_trace.sh
+++ workload_generation/gen_trace.sh
@@ -5,14 +5,17 @@ _script="$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
 EXTRAE_HOME=/opt/extrae
 NPB_BIN=/root/NPB3.3.1/NPB3.3-MPI/bin/
 
+NBPROC=$(uniq $OAR_NODEFILE | wc -l)
+CMDS=$(ls -1 $NPB_BIN | grep -E "*\.$NBPROC$")
+RESULTS=/root/results/
+
+mkdir -p $RESULTS
+cd $RESULTS
+
 export LD_PRELOAD=$EXTRAE_HOME/lib/libmpitrace.so
 export export EXTRAE_CONFIG_FILE=$_script/extrae.xml
-
-NBPROC=2
-CMDS=$(ls -1 $NPB_BIN | grep -E "*\.$NBPROC$")
-
 for cmd in $CMDS
 do
-  mpirun -np $NBPROC --mca orte_rsh_agent "oarsh" -machinefile $OAR_NODEFILE $cmd
+  mpirun -np $NBPROC --mca orte_rsh_agent "oarsh" -machinefile $OAR_NODEFILE $NPB_BIN/$cmd
   $_script/prv2tit.pl -i $cmd > $cmd.tit
 done
