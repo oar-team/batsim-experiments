3ccf2758ebc679f725719a291c06ea3884f16234
Michael Mercier <michael.mercier@inria.fr>
2016-02-04 19:56:57 +0100

    Fix mpi_bench (still testing)

--- workload_generation/mpi_bench.py
+++ workload_generation/mpi_bench.py
@@ -10,6 +10,7 @@ Only one MPI process is running by node to be consistent with Simgrid
 simple computation modele.
 """
 import os
+import ipdb
 from execo import SshProcess, format_date
 from execo_g5k import oarsub, oardel, OarSubmission, \
     get_oar_job_nodes, wait_oar_job_start, \
@@ -17,7 +18,7 @@ from execo_g5k import oarsub, oardel, OarSubmission, \
 from execo_g5k.kadeploy import deploy, Deployment
 from execo_engine import Engine, ParamSweeper, sweep, slugify, logger
 
-script_path = os.path.realpath(__file__)
+script_path = os.path.dirname(os.path.realpath(__file__))
 
 
 def prediction_callback(ts):
@@ -34,7 +35,7 @@ class mpi_bench(Engine):
                        '-x EXTRAE_CONFIG_FILE=' + script_path + '/extrae.xml'
         cluster = 'graphene'
         switch = 'sgraphene4'
-        env_name = "~/my_g5k_images/debian8_workload_generation.dsc"
+        env_name = "debian8_workload_generation_nfs"
         self.result_dir = script_path + '/results'
         npb_bin_path = script_path + '/NPB_bin'
 
@@ -56,58 +57,65 @@ class mpi_bench(Engine):
             'bench': ['lu']
         })
 
-        logger.info('Number of parameters combinations',
-                    len(self.sweeper.get_remaining()))
+        logger.info('Number of parameters combinations {}'.format(
+            str(len(self.sweeper.get_remaining()))))
 
-        # Iterate over the parameters and execute the bench
-        while len(self.sweeper.get_remaining()) > 0:
-            comb = self.sweeper.get_next()
-            logger.info('Processing new combination %s' % (comb,))
-            site = get_cluster_site(cluster)
-            jobs = oarsub([(OarSubmission(resources="{switch='" +
-                                          switch + "'}",
-                                          job_type='allow_classic_ssh',
-                                          walltime='4:00'),
-                            site)])
+        site = get_cluster_site(cluster)
+        jobs = oarsub([(OarSubmission(resources="/nodes=2",
+                                      job_type='deploy',
+                                      walltime='04:00:00'),
+                        site)])
 
-            if jobs[0][0]:
-                try:
-                    wait_oar_job_start(*jobs[0])
-                    nodes = get_oar_job_nodes(*jobs[0])
+        job_id, site = jobs[0]
+        if job_id:
+            try:
+                logger.info("waiting job start %s on %s" % (job_id, site))
+                wait_oar_job_start(job_id, site, prediction_callback = prediction_callback)
+                logger.info("getting nodes of %s on %s" % (job_id, site))
+                nodes = get_oar_job_nodes(job_id, site)
 
-                    logger.info("deploying nodes: ", str(nodes))
-                    deployed, undeployed = deploy(
-                        Deployment(nodes, env_name=env_name))
+                logger.info("deploying nodes: {}".format(str(nodes)))
+                deployed, undeployed = deploy(
+                    Deployment(nodes, env_name=env_name))
+                if undeployed:
+                    logger.warn("NOT deployed nodes: {}".format(str(undeployed)))
+                    raise RuntimeError('Deployement failed')
 
-                    hostfile_filename = 'hostfile-'.join(comb['nb_nodes'])
-                    with open(hostfile_filename) as hostfile:
+                # Iterate over the parameters and execute the bench
+                while len(self.sweeper.get_remaining()) > 0:
+                    comb = self.sweeper.get_next()
+                    logger.info('Processing new combination %s' % (comb,))
+
+                    hostfile_filename = self.result_dir + '/' + 'hostfile-' + comb['nb_nodes']
+                    with open(hostfile_filename, 'w') as hostfile:
                         for node in nodes:
-                            print>>hostfile, node
+                            print>>hostfile, node.address
 
                     mpi_command = '{}/{}.{}.{}'.format(npb_bin_path,
                                                        comb['bench'],
                                                        comb['size'],
                                                        comb['nb_nodes'])
 
-                    bench_cmd = 'mpirun {option}'
-                    '-hostfile {hostfile} {env_vars}'
-                    '{command}'.format(option=mpi_options,
+                    bench_cmd = ('mpirun {option} -hostfile {hostfile} '
+                            '{env_vars} {command}').format(option=mpi_options,
                                        hostfile=hostfile_filename,
                                        env_vars=mpi_env_vars,
                                        command=mpi_command)
                     lu_bench = SshProcess(bench_cmd, nodes[0])
                     lu_bench.stdout_handlers.append(
                         self.result_dir + '/' + slugify(comb) + '.out')
-                    logger.info("generate command: ", bench_cmd)
+                    logger.info("generate command: {}".format(bench_cmd))
                     lu_bench.run()
                     if lu_bench.ok:
                         logger.info("comb ok: %s" % (comb,))
                         self.sweeper.done(comb)
                         continue
-                finally:
-                    oardel(jobs)
-            logger.info("comb NOT ok: %s" % (comb,))
-            self.sweeper.cancel(comb)
+            finally:
+                ipdb.set_trace()
+                logger.info("deleting job {}".format(jobs))
+                oardel(jobs)
+        logger.info("comb NOT ok: %s" % (comb,))
+        self.sweeper.cancel(comb)
 
 if __name__ == "__main__":
     engine = mpi_bench()
