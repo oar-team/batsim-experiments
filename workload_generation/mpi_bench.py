c075ebe93ae15ae8d6f9ef06d24054be3fe38d67
Michael Mercier <michael.mercier@inria.fr>
2016-02-05 15:50:57 +0100

    proper management and export result of mpi_bench

--- workload_generation/mpi_bench.py
+++ workload_generation/mpi_bench.py
@@ -10,7 +10,10 @@ Only one MPI process is running by node to be consistent with Simgrid
 simple computation modele.
 """
 import os
+import shutil
 import ipdb
+import traceback
+import time
 from execo import SshProcess, format_date
 from execo_g5k import oarsub, oardel, OarSubmission, \
     get_oar_job_nodes, wait_oar_job_start, \
@@ -27,6 +30,9 @@ def prediction_callback(ts):
 
 class mpi_bench(Engine):
 
+    def setup_result_dir(self):
+        self.result_dir = script_path + '/results_' + time.strftime("%Y-%m-%d--%H-%M-%S")
+
     def run(self):
         """Inherited method, put here the code for running the engine"""
         # define parameter consistent with the kadeploy image
@@ -36,12 +42,13 @@ class mpi_bench(Engine):
         cluster = 'graphene'
         switch = 'sgraphene4'
         env_name = "debian8_workload_generation_nfs"
-        self.result_dir = script_path + '/results'
         npb_bin_path = script_path + '/NPB_bin'
 
         # define the parameters
+        nb_nodes = ['2', '4', '8', '16', '32']
+        #nb_nodes = ['2']
         self.parameters = {
-            'nb_nodes': ['2', '4', '8', '16', '32'],
+            'nb_nodes': nb_nodes,
             'size': ['B', 'C', 'D'],
             'bench': ['is', 'ft', 'lu']
         }
@@ -60,6 +67,9 @@ class mpi_bench(Engine):
         logger.info('Number of parameters combinations {}'.format(
             str(len(self.sweeper.get_remaining()))))
 
+        # go to the result folder before everything
+        os.chdir(self.result_dir)
+
         site = get_cluster_site(cluster)
         #jobs = oarsub([(OarSubmission(resources="/nodes=2",
         jobs = oarsub([(OarSubmission(resources="{switch='" + switch + "'}/switch=1",
@@ -74,6 +84,13 @@ class mpi_bench(Engine):
                 wait_oar_job_start(job_id, site, prediction_callback = prediction_callback)
                 logger.info("getting nodes of %s on %s" % (job_id, site))
                 nodes = get_oar_job_nodes(job_id, site)
+                logger.info("create hostfiles for all combinations")
+                for node_number in nb_nodes:
+                    hostfile_filename = self.result_dir + '/' + 'hostfile-' + node_number
+                    with open(hostfile_filename, 'w') as hostfile:
+                        for node in nodes[:int(node_number)]:
+                            print>>hostfile, node.address
+
 
                 logger.info("deploying nodes: {}".format(str(nodes)))
                 deployed, undeployed = deploy(
@@ -87,22 +104,18 @@ class mpi_bench(Engine):
                     comb = self.sweeper.get_next()
                     logger.info('Processing new combination %s' % (comb,))
 
+                    mpi_command = '{}.{}.{}'.format(comb['bench'],
+                                                    comb['size'],
+                                                    comb['nb_nodes'])
+
                     hostfile_filename = self.result_dir + '/' + 'hostfile-' + comb['nb_nodes']
-                    with open(hostfile_filename, 'w') as hostfile:
-                        for node in nodes:
-                            print>>hostfile, node.address
-
-                    mpi_command = '{}/{}.{}.{}'.format(npb_bin_path,
-                                                       comb['bench'],
-                                                       comb['size'],
-                                                       comb['nb_nodes'])
-
                     bench_cmd = ('mpirun {option} -hostfile {hostfile} '
                             '{env_vars} {command}').format(option=mpi_options,
                                        hostfile=hostfile_filename,
                                        env_vars=mpi_env_vars,
-                                       command=mpi_command)
-                    lu_bench = SshProcess(bench_cmd, nodes[0])
+                                       command=npb_bin_path + "/" + mpi_command)
+                    lu_bench = SshProcess("cd " + self.result_dir +
+                                               "; " + bench_cmd, nodes[0])
                     lu_bench.stdout_handlers.append(
                         self.result_dir + '/' + slugify(comb) + '.out')
                     logger.info("generate command: {}".format(bench_cmd))
@@ -110,13 +123,19 @@ class mpi_bench(Engine):
                     if lu_bench.ok:
                         logger.info("comb ok: %s" % (comb,))
                         self.sweeper.done(comb)
+                        # save results
+                        # for extension in ['pcf', 'prv', 'row']:
+                        #     filename = mpi_command + '.' + extension
+                        #     shutil.move(filename, self.result_dir)
                         continue
-            finally:
+            except Exception as exception:
+                traceback.print_exc()
+                logger.warn("comb NOT ok: %s" % (comb,))
+                self.sweeper.cancel(comb)
                 ipdb.set_trace()
-                logger.info("deleting job {}".format(jobs))
-                oardel(jobs)
-        logger.info("comb NOT ok: %s" % (comb,))
-        self.sweeper.cancel(comb)
+            finally:
+                logger.info("to delete job:\noardel {}".format(jobs))
+                #oardel(jobs)
 
 if __name__ == "__main__":
     engine = mpi_bench()
