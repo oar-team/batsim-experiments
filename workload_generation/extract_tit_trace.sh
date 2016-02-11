8615deddc1afef2384969d76f8e1e1a04df353d3
Michael Mercier <michael.mercier@inria.fr>
2016-02-11 15:38:05 +0100

    Extract tit trace using parallel

--- workload_generation/extract_tit_trace.sh
+++ workload_generation/extract_tit_trace.sh
@@ -5,13 +5,18 @@ _script="$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
 EXTRAE_TRACE_DIR=/home/mmercier/expe-batsim/workload_generation/results_2016-02-08--10-10-44
 TIT_TRACE_DIR=/home/mmercier/expe-batsim/workload_generation/results_2016-02-08--10-10-44/tit
 CMDS=$(ls -1 $EXTRAE_TRACE_DIR | grep prv | xargs -I{} basename {} .prv)
+TO_RUN=/tmp/to_run
 
+rm $TO_RUN
 mkdir -p $TIT_TRACE_DIR
 
-i=0;
-waitevery=$(nproc);
 for cmd in $CMDS
 do
-  $_script/prv2tit.pl -i $EXTRAE_TRACE_DIR/$cmd > $TIT_TRACE_DIR/$cmd.tit & (( i++%waitevery==0 )) && wait
+  # Do not erase already computed tit files
+  if [ ! -f $TIT_TRACE_DIR/$cmd.tit ] || [ ! -s $TIT_TRACE_DIR/$cmd.tit ]
+  then
+    echo "$_script/prv2tit.pl -i $EXTRAE_TRACE_DIR/$cmd > $TIT_TRACE_DIR/$cmd.tit" >> $TO_RUN
+  fi
 done
-wait
+
+cat $TO_RUN | parallel
