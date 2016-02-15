5b0f815bbef455f730e79372490db496a77e9421
Millian Poquet <millian.poquet@inria.fr>
2016-02-15 19:11:30 +0100

    Added micro workloads

---
+++ workload_generation/generate_micro_workload.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+for i in {0..9}
+do
+    python json_workload_generator.py -i 4 profiles/delay_profiles_mpi_commands.json "generated_workloads/micro_workload${i}.json"  -jn 1 -lambda 1 -k 20 -mu 0 -sigma 0.5 -rs ${i}
+done
