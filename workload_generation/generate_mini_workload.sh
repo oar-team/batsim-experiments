81943c3433d7d69a4ebc2fa53281ac126553ff3e
Millian Poquet <millian.poquet@inria.fr>
2016-02-17 20:51:08 +0100

    Workload generation update: mpi hostfile & nb_res

--- workload_generation/generate_mini_workload.sh
+++ workload_generation/generate_mini_workload.sh
@@ -2,5 +2,5 @@
 
 for i in {0..9}
 do
-    python json_workload_generator.py -i 4 profiles/delay_profiles_mpi_commands.json "generated_workloads/mini_workload${i}.json"  -jn 20 -lambda 1 -k 20 -mu 0.3 -sigma 0.5 -rs ${i}
+    python json_workload_generator.py -i 4 profiles/delay_profiles_mpi_commands.json "generated_workloads/mini_workload${i}.json" 37 -jn 20 -lambda 1 -k 20 -mu 0.3 -sigma 0.5 -rs ${i}
 done
---
+++ workload_generation/generate_mini_workload_msg.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+for i in {0..9}
+do
+    python json_workload_generator.py -i 4 ../job_calibration/workload_all_sizes_seq_calibrated.json "generated_workloads/mini_workload_msg${i}.json" 37 -jn 20 -lambda 1 -k 20 -mu 0.3 -sigma 0.5 -rs ${i}
+done
