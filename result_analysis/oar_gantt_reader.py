7a288dde25ca8acf7c83579c43554c8ff052a9cb
Millian Poquet <millian.poquet@inria.fr>
2016-02-17 15:59:24 +0100

    OAR Gantt reader: should now read detailed output

--- result_analysis/oar_gantt_reader.py
+++ result_analysis/oar_gantt_reader.py
@@ -18,6 +18,7 @@ import datetime
 # Program parameters parsing
 parser = argparse.ArgumentParser(description='Reads an OAR Gantt JSON file containing and extracts information about the jobs it contains')
 parser.add_argument('inputJSON', type=argparse.FileType('r'), help='The input JSON OAR Gannt file')
+parser.add_argument("-d", "--details", action="store_true", help="If set, the input JSON is expected to be a detailed output (containing more fields)")
 
 args = parser.parse_args()
 
@@ -29,12 +30,21 @@ jobs = input_json_data['jobs']
 terminated_jobs = []
 error_jobs = []
 timeout_jobs = []
+zero_exit_code_jobs = []
+bad_exit_code_jobs = []
 
 for job in jobs:
     job_id = int(job)
     job_state = jobs[job]['state']
     job_limit_stop_time = float(jobs[job]['limit_stop_time'])
     job_stop_time = float(jobs[job]['stop_time'])
+
+    if args.details:
+        job_exit_code = int(jobs[job]['exit_code'])
+        if job_exit_code == 0:
+            zero_exit_code_jobs.append(job)
+        else:
+            bad_exit_code_jobs.append(job)
 
     if job_stop_time >= job_limit_stop_time:
         timeout_jobs.append(job)
@@ -89,6 +99,15 @@ print('Terminated jobs not finished: {}, [{}]'.format(len(terminated_jobs_not_fi
 
 print('Terminated finished jobs: {}, [{}]'.format(len(terminated_jobs_finished), ','.join(terminated_jobs_finished)))
 print('Terminated finished jobs that have a stderr: {}, [{}]'.format(len(terminated_finished_jobs_with_stderr), ','.join(terminated_finished_jobs_with_stderr)))
+
+if args.details:
+    zero_exit_code_not_finished_jobs = list(set(zero_exit_code_jobs) - set(terminated_jobs_finished))
+    finished_bad_exit_code_jobs = list(set(terminated_jobs_finished) - set(zero_exit_code_jobs))
+
+    print()
+    print('Jobs with good exit_code: {}, [{}]'.format(len(zero_exit_code_jobs), ','.join(zero_exit_code_jobs)))
+    print('Jobs with good exit code that did not terminate&finish: {}, [{}]'.format(len(zero_exit_code_not_finished_jobs), ','.join(zero_exit_code_not_finished_jobs)))
+    print('Jobs terminated&finished with bad exit code: {}, [{}]'.format(len(finished_bad_exit_code_jobs), ','.join(finished_bad_exit_code_jobs)))
 
 #print('\n'.join(['{}: [{}]'.format(ok_job, ','.join([str(r) for r in jobs[ok_job]['resources']])) for ok_job in terminated_jobs_no_stderr]))
 
