ecf71a6feede6fcbfc0e889fca9c39e3947255ed
Millian Poquet <millian.poquet@inria.fr>
2016-04-21 22:37:59 +0200

    Execution of configured experiment

---
+++ simulated_experiment/launch_expe_config.py
@@ -0,0 +1,180 @@
+#!/usr/bin/python3
+
+import argparse
+import json
+import os
+import subprocess
+import shutil
+import time
+
+def create_dir_if_not_exists(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def remove_file_if_exists(filename):
+    if os.path.exists(filename):
+        os.remove(filename)
+
+def remove_dir_if_exists(directory):
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
+
+def run_subprocess_do_not_wait(command, working_directory, stdout_file, stderr_file):
+    return subprocess.Popen(command,
+                            cwd = working_directory,
+                            stdout=stdout_file,
+                            stderr=stderr_file)
+
+def wait_for_termination(process, timeout=None):
+    (stdout, stderr) = process.communicate(timeout=timeout)
+
+# def run_write_output(command, working_directory, stdout_filename, stderr_filename, timeout=None):
+#     process = run_subprocess_do_not_wait(command, working_directory)
+#     wait_for_termination_output(process, stdout_filename, stderr_filename, timeout)
+
+#     return process.returncode
+
+def socket_in_use(sock):
+    return sock in open('/proc/net/unix').read()
+
+def wait_for_batsim_to_open_connection(sock='/tmp/bat_socket', timeout=60, seconds_to_sleep=0.1):
+    while timeout > 0 and not socket_in_use(sock):
+        time.sleep(seconds_to_sleep)
+    return timeout > 0
+
+def launch_experiment(config_json_filename):
+    json_file = open(config_json_filename, 'r')
+    json_data = json.load(json_file)
+
+    output_directory = str(json_data['output_directory'])
+    create_dir_if_not_exists(output_directory)
+
+    default_timeout = None
+    if 'default_timeout' in json_data:
+        default_timeout = float(json_data['default_timeout'])
+
+    default_socket_creation_timeout = 60
+    if 'default_socket_creation_timeout' in json_data:
+        default_socket_creation_timeout = float(json_data['default_socket_creation_timeout'])
+
+    default_socket = '/tmp/bat_socket'
+    if 'default_socket' in json_data:
+        default_socket = str(json_data['default_socket'])
+
+    base_directory = os.getcwd()
+    os.chdir(os.getcwd() + '/' + output_directory)
+    experiment_base_directory = os.getcwd()
+
+    aggregate_results = False
+    if 'aggregate_results' in json_data:
+        aggregate_results = bool(json_data['aggregate_results'])
+
+    nb_experiments = len(json_data['experiments'])
+    curr_experiment = 1
+    failed_experiments = []
+
+    for experiment_name in json_data['experiments']:
+        # Let's load what is needed to run the experiment
+        experiment = json_data['experiments'][experiment_name]
+
+        simulated_workload = str(experiment['simulated_workload'])
+        simulated_platform = str(experiment['simulated_platform'])
+
+        batsim_command_str = str(experiment['batsim_command'])
+        sched_command_str = str(experiment['sched_command'])
+
+        timeout = default_timeout
+        if 'timeout' in experiment:
+            timeout = float(experiment['timeout'])
+
+        socket_creation_timeout = default_socket_creation_timeout
+        if 'socket_creation_timeout' in experiment:
+            socket_creation_timeout = float(experiment['socket_creation_timeout'])
+
+        socket = default_socket
+        if 'socket' in experiment:
+            socket = str(experiment['socket'])
+
+        # Let's replace some strings in the commands!
+        batsim_command_str = batsim_command_str.replace('PLATFORM', base_directory + '/' + simulated_platform)
+        batsim_command_str = batsim_command_str.replace('WORKLOAD', base_directory + '/' + simulated_workload)
+
+        sched_command_str = sched_command_str.replace('PLATFORM', base_directory + '/' + simulated_platform)
+        sched_command_str = sched_command_str.replace('WORKLOAD', base_directory + '/' + simulated_workload)
+
+        # Let's create the real commands
+        batsim_command = batsim_command_str.split(' ')
+        sched_command = sched_command_str.split(' ')
+
+        # Directory handling
+        os.chdir(experiment_base_directory)
+        remove_dir_if_exists(experiment_name)
+        create_dir_if_not_exists(experiment_name)
+        os.chdir(os.getcwd() + '/' + experiment_name)
+
+        # Let's run the experiment
+        batsim_stdout_file = open(os.getcwd() + '/batsim.stdout', 'w')
+        batsim_stderr_file = open(os.getcwd() + '/batsim.stderr', 'w')
+        sched_stdout_file = open(os.getcwd() + '/sched.stdout', 'w')
+        sched_stderr_file = open(os.getcwd() + '/sched.stderr', 'w')
+        commands_file = open(os.getcwd() + '/commands.log', 'w')
+        commands_file.write('batsim : [{}]\n\n'.format(', '.join(batsim_command)))
+        commands_file.write('batsim_str : {}\n\n'.format(' '.join(batsim_command)))
+        commands_file.write('sched : [{}]\n\n'.format(', '.join(sched_command)))
+        commands_file.write('sched_str : {}\n\n'.format(' '.join(sched_command)))
+        commands_file.close()
+        print('Starting experiment {}/{} ({} %): {}'.format(curr_experiment,
+                                                            nb_experiments,
+                                                            (curr_experiment * 100) / nb_experiments,
+                                                            experiment_name))
+        remove_file_if_exists(socket)
+        batsim_process = run_subprocess_do_not_wait(command = batsim_command,
+                                                    working_directory = os.getcwd(),
+                                                    stdout_file = batsim_stdout_file,
+                                                    stderr_file = batsim_stderr_file)
+        if wait_for_batsim_to_open_connection(timeout=socket_creation_timeout):
+            sched_process = run_subprocess_do_not_wait(command = sched_command,
+                                                       working_directory = os.getcwd(),
+                                                       stdout_file = sched_stdout_file,
+                                                       stderr_file = sched_stderr_file)
+            try:
+                wait_for_termination(sched_process, timeout = timeout)
+                wait_for_termination(batsim_process, timeout = timeout)
+
+                # Let's check return values
+                if (sched_process.returncode != 0) or (batsim_process.returncode != 0):
+                    print('FAILED: bad returncode (batsim={}, sched={})'.format(batsim_process.returncode,
+                                                                                sched_process.returncode))
+                    failed_experiments.append(experiment_name)
+            except subprocess.TimeoutExpired:
+                print('FAILED: timeout reached :(')
+                failed_experiments.append(experiment_name)
+            sched_process.kill()
+        else:
+            print('FAILED: cannot reach batsim socket creation :(')
+        batsim_process.kill()
+
+        batsim_stdout_file.close()
+        batsim_stderr_file.close()
+        sched_stdout_file.close()
+        sched_stderr_file.close()
+
+        curr_experiment += 1
+
+    print('All experiments have been executed')
+    if len(failed_experiments) == 0:
+        print('Everything seemed to be executed correctly')
+    else:
+        print('{} experiments failed : [{}]'.format(len(failed_experiments), ', '.join(failed_experiments)))
+
+def main():
+    # Program parameters parsing
+    parser = argparse.ArgumentParser(description='Launches an experiment according to a configuration file')
+    parser.add_argument('experiment_config_json', type=str,
+                        help='The input configuration JSON file which describes the experiment to run')
+
+    args = parser.parse_args()
+    launch_experiment(args.experiment_config_json)
+
+if __name__ == '__main__':
+    main()
