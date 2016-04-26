ff57db7151e3c1b245a5b45e82c524eb213a7797
Millian Poquet <millian.poquet@inria.fr>
2016-04-26 18:14:02 +0200

    Workload update

--- workload_generation/generate_g5k_workloads_2016-04-26.sh
+++ workload_generation/generate_g5k_workloads_2016-04-26.sh
@@ -9,9 +9,9 @@ do
     for random_seed in {0..9}
     do
         python json_workload_generator.py -i 4 profiles/delay_profiles_mpi_commands.json \
-            "${output_dir}/g5k_micro_workload_delay_seed${random_seed}_size${platform_size}.json" ${platform_size} \
-            -jn 700 -lambda 16 -k 4 -mu 0.25 -sigma 0.5 -rs ${random_seed} --maximum_job_length 600
+            "${output_dir}/g5k_workload_delay_seed${random_seed}_size${platform_size}.json" ${platform_size} \
+            -jn 800 -lambda 16 -k 4 -mu 0.25 -sigma 0.5 -rs ${random_seed} --maximum_job_length 600
 
-        cp "${output_dir}/g5k_micro_workload_delay_seed${random_seed}_size${platform_size}.json" ../simulated_experiment/workloads
+        cp "${output_dir}/g5k_workload_delay_seed${random_seed}_size${platform_size}.json" ../simulated_experiment/workloads
     done
 done
